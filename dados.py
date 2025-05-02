4637f49dcdddf9ae3fbf9fa9e055b5cb3cc7d774
Guilherme Caldas <96866742+ggcds@users.noreply.github.com>
2025-05-02 18:54:47 -0300

    Add files via upload

--- dados.py
+++ dados.py
@@ -1,21 +1,21 @@
 SEU_NOME = {
-    "Nome": "PREENCHER  ", # Campo de texto
+    "Nome": "PREENCHER", # Campo de texto
     "Sobrenome": "PREENCHER", # Campo de texto
     "E-mail": "PREENCHER", # Campo de texto
     "Telefone": "PREENCHER", # Campo de texto
     "Currículo": "files/curriculo.pdf", # Adicionar seu curriculo no caminho especificado com o nome curriculo.pdf
     "LinkedIn": "PREENCHER", # Campo de texto
     "Website/Portfólio": "PREENCHER", # Campo de texto
-    "Quais são os seus pronomes?": "Ele/Dele", # Opcoes: Ele/Dele, Ela/Dela, Ile/Dile, Elu/Delu
-    "Onde você descobriu a vaga?": "Site de carreiras", # Opcoes: LinkedIn, Instagram, Indeed, Glassdoor, Indicação, Podcast, Medium, Site de carreiras, Nenhuma das alternativas acima
-    "Você já trabalhou no iFood?": "Não", # Opcoes: Sim, Não
-    "Com qual gênero você se identifica?": "Homem cis", # Opcoes: Homem cis, Mulher cis, Homen trans, Mulher trans, Não-binário, Travesti, Outra, Não quero responder
-    "Qual sua orientação sexual?": "Heterossexual", # Opcoes: Assexual, Bissexual, Homossexual, Heterossexual, Pansexual, Outras, Não quero responder
-    "Qual é sua cor ou raça?": "Branco", # Opcoes: Amarelo, Indígena, Branco, Pardo, Preta, Não quero responder
-    "Você é uma pessoa com deficiência?": "Não", # Opcoes: Sim, Não, Não quero responder
-    "Se você é uma pessoa com deficiência, por favor, nos informe qual é a sua deficiência:": "Não Aplicável", # Opcoes: Visual, Física, Auditiva, Mental, Intelectual, Psicossocial, Outra, Não Aplicável, Não quero responder
-    "Você está ciente de que respondendo esse formulário de diversidade, você aceita o Aviso de Diversidade – D&I?": "Li e aceito", # Opcoes: Li e aceito, Não quero responder
-    "Possui experiência com planejamento de s&op?": "Sim" # Campo de texto
+    "Quais são os seus pronomes?": "PREENCHER", # Opcoes: Ele/Dele, Ela/Dela, Ile/Dile, Elu/Delu
+    "Onde você descobriu a vaga?": "PREENCHER", # Opcoes: LinkedIn, Instagram, Indeed, Glassdoor, Indicação, Podcast, Medium, Site de carreiras, Nenhuma das alternativas acima
+    "Você já trabalhou no iFood?": "PREENCHER", # Opcoes: Sim, Não
+    "Com qual gênero você se identifica?": "PREENCHER", # Opcoes: Homem cis, Mulher cis, Homen trans, Mulher trans, Não-binário, Travesti, Outra, Não quero responder
+    "Qual sua orientação sexual?": "PREENCHER", # Opcoes: Assexual, Bissexual, Homossexual, Heterossexual, Pansexual, Outras, Não quero responder
+    "Qual é sua cor ou raça?": "PREENCHER", # Opcoes: Amarelo, Indígena, Branco, Pardo, Preta, Não quero responder
+    "Você é uma pessoa com deficiência?": "PREENCHER", # Opcoes: Sim, Não, Não quero responder
+    "Se você é uma pessoa com deficiência, por favor, nos informe qual é a sua deficiência:": "PREENCHER", # Opcoes: Visual, Física, Auditiva, Mental, Intelectual, Psicossocial, Outra, Não Aplicável, Não quero responder
+    "Você está ciente de que respondendo esse formulário de diversidade, você aceita o Aviso de Diversidade – D&I?": "PREENCHER", # Opcoes: Li e aceito, Não quero responder
+    "Possui experiência com planejamento de s&op?": "PREENCHER" # Campo de texto
 }
 
 # Banco
